3ce6eeaaf2a31c86a308b7df99847c7261f6d088
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-01-12 17:04:14 -0500

    add

--- Learning_Tkinter.py
+++ Learning_Tkinter.py
@@ -22,19 +22,22 @@ class ParentWindow(Frame):
         self.varFName = StringVar()
         self.varLName = StringVar()
 
-        self.lblFName = Label(self.master,text='First Name: ', font=("Helvetica", 16), fg='black', bg='lightblue' )
-        self.lblFName.grid(row=0, column=0)
+        self.lblFName = Label(self.master,text='First Name: ', font=("Helvetica", 16), fg='black', bg='lightgray' )
+        self.lblFName.grid(row=0, column=0,padx=(30,0), pady=(30,0))
 
-        self.lblLName = Label(self.master,text='Last Name: ', font=("Helvetica", 16), fg='black', bg='lightblue' )
-        self.lblLName.grid(row=1, column=0)
+        self.lblLName = Label(self.master,text='Last Name: ', font=("Helvetica", 16), fg='black', bg='lightgray' )
+        self.lblLName.grid(row=1, column=0,padx=(30,0), pady=(30,0))
 
        
         self.txtFName = Entry(self.master,text=self.varFName, font=("Helvetica", 16), fg='black', bg='lightblue')
-        self.txtFName.grid(row=, column=)
+        self.txtFName.grid(row=0, column=1,padx=(30,0), pady=(30,0))
 
         
         self.txtLName = Entry(self.master,text=self.varLName, font=("Helvetica", 16), fg='black', bg='lightblue')
-        self.txtLName.grid(row=, column=) 
+        self.txtLName.grid(row=1, column=1,padx=(30,0), pady=(30,0))
+
+        self.btnSubmit = Button(self.master, text="Submit", width=10, height=2)
+        self.btnSubmit.grid(row=2, column=1,padx=(0,0), pady=(30,0), sticky=NE)
 
 
 
--- Polymorphism_Assignment.py
+++ Polymorphism_Assignment.py
@@ -28,23 +28,23 @@ class FTEmployee(User):
         entry_department_number = input("Enter your Department Number: ")
         entry_pin = input("Enter your pin: ")
         if (entry_department_number == self.department_number and entry_pin == self.pin_number):
-            print("Welcome back, {}!".format(entry_name))
+            print("Welcome back, {}!".format(employee_name))
         else:
             print("Your Department Number or pin is incorrect.  Please re-enter.")
 
 class Customer(User):
-    Customer_Email = "Jane@hotmail.com"
-    Member_ID = "444333"
+    customer_email = "Jane@hotmail.com"
+    member_id = "444333"
     
 
 #This is the same method in the parent class "User".
 #The difference is that, instead of using entry_password, we're using entry_pin.
 
     def getLoginInfo(self):
-        Customer_Email = input("Enter your email: ")
-        entry_ID = input("Enter your Member ID: ")
-        if (Customer_Email == self.email and entry_id == self.member_id):
-            print("Welcome back, {}!".format(entry_name))
+        customer_email = input("Enter your email: ")
+        entry_id = input("Enter your Member ID: ")
+        if (entry_email == self.email and entry_id == self.member_id):
+            print("Welcome back, Member {}!".format(member_id))
         else:
             print("Your Email or Member ID is incorrect.")
 

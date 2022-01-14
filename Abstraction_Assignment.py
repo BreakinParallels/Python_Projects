89fa0dbb50ae81e4c63d5dddc3146d65b74a06fc
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-01-13 19:00:01 -0500

    add 2 files ab

---
+++ Abstraction_Assignment.py
@@ -0,0 +1,28 @@
+
+
+
+from abc import ABC, abstractmethod
+class tv(ABC):
+    def creditLimit(self, amount):
+        print("Your credit card purchase is: ",amount)
+#this function is telling us to pass in an agrument,
+#but we won't tell you how or what kind
+#of data it will be.
+    @abstractmethod
+    def payment(self, amount):
+        pass
+
+
+class CreditCardPayment(tv):
+#here we've defined how to implement the payment function
+#from its parent paySlip class.
+    def payment(self, amount):
+        print('Your purchase amount of {} exceeded your $1000 limit '.format(amount))
+
+
+obj = DebitCardPayment()
+obj.creditLimit("$3000")
+obj.payment("$2000")
+
+
+    
